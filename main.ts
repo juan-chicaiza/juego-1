dd2f4aa0b28bc747250a38c88f390534fa6b9f0e
juan-chicaiza <118471244+juan-chicaiza@users.noreply.github.com>
2022-11-17 14:48:02 -0500

    Archivos iniciales del proyecto de MakeCode

---
+++ main.ts
@@ -0,0 +1,71 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.knock.play()
+    mySprite.vy = -300
+})
+info.onCountdownEnd(function () {
+    game.over(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    music.powerUp.play()
+    info.changeLifeBy(-1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    music.powerUp.play()
+    info.changeScoreBy(1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.confetti, 500)
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(11)
+tiles.setCurrentTilemap(tilemap`level1`)
+info.setLife(5)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.ay = 800
+scene.cameraFollowSprite(mySprite)
+tiles.placeOnRandomTile(mySprite, sprites.builtin.oceanDepths9)
+let mySprite2 = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . . . 
+    c c c c c d d e e e f c . . . . 
+    . f d d d d e e e f f . . . . . 
+    . . f f f f f e e e e f . . . . 
+    . . . . f f e e e e e e f . f f 
+    . . . f e e f e e f e e f . e f 
+    . . f e e f e e f e e e f . e f 
+    . f b d f d b f b b f e f f e f 
+    . f d d f d d f d d b e f f f f 
+    . . f f f f f f f f f f f f f . 
+    `, SpriteKind.Enemy)
+tiles.placeOnRandomTile(mySprite2, sprites.dungeon.hazardLava0)
+mySprite2.follow(mySprite, 50)
+info.startCountdown(80)
